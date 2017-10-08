a606f570b4c08bc020a81633a89e64aa4a9ec276
lollipop <wangrongwei2014@gmail.com>
2017-10-08 15:20:54 +0800

    调试生成的魔方问题生成程序---ok

--- magic_square.c
+++ magic_square.c
@@ -6,16 +6,73 @@
 
 #include <stdio.h>
 
+#define MAX 15
+void magic(int n);
 
 int main(int argc,char *argv[])
 {
-	
+	int num=0;
+	printf("enter your magic square num:");
+	//while(1)
+	scanf("%d",&num);
+	if(num < 1 || num >15){
+		printf("please enter right magic num!!!\n");
+		return 0;
+	}
+	else
+		magic(num);
 
+	return 0;
 }
-
-int magic(int n)
+/*
+ * 参数n:魔方的阶数
+ */
+void magic(int n)
 {
-	
+	int i=0,j,row,col;
+	int magic_t[MAX][MAX]={0};
+	//这里的col <= n多余了
+	for(col=n/2,row=0;col<=n;col--,row--){
+		if(row==-1){
+			row = n-1;
+		}
+		if(col==-1){
+			col = n-1;
+		}
+		i++;
+		if(i<=n*n){
+			//检测是不是0，不是0需要重新计算放置位置
+			if(magic_t[row][col] == 0)
+				magic_t[row][col] = i;
+			else{
+				if((row+2)<=n-1 || (col+1)<=n-1){
+					row = row + 2;
+					col = col + 1;
+				}
+				else{
+					if(row==n-1 || col==n-1){
+						if(row==n-1)
+							row = 0;
+						if(col==n-1)
+							col = 0;
+					}
+					row = row + 1;
+
+				}
+
+				magic_t[row][col] = i;
+			}
+		}
+		else
+			break;
+
+	}
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			printf("%3d",magic_t[i][j]);
+		}
+		printf("\n");
+	}
 
 }
 
